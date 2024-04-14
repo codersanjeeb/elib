cf82e6e65ecd2288beda815021b33a8c4e8eec9d
codersanjeeb <codersanjeeb@gmail.com>
2024-04-14 21:40:42 +0530

    create user route

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,17 @@ import express, { NextFunction, Request, Response } from "express";
 import createHttpError, { HttpError } from "http-errors";
 import { config } from "./config/config";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express();
 
 //Routes
 
 app.get("/", (req, res, next) => {
-  const error = createHttpError(400, "Something went wrong");
-  throw error;
   res.json({ message: "Welcome to eLib" });
 });
+
+app.use("/api/users",userRouter);
 
 //Global error handler.
 app.use(globalErrorHandler);
